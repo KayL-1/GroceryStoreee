229b833795c0f809516437becfb234d5968db669
KayL-1 <71209263+KayL-1@users.noreply.github.com>
2023-04-28 14:40:43 +0800

    quite a fix hehe
    pls pls pls

--- GroceryApp/app/src/main/java/com/example/groceryapp/WelcomeActivity.java
+++ GroceryApp/app/src/main/java/com/example/groceryapp/WelcomeActivity.java
@@ -4,41 +4,43 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.content.Intent;
+import android.app.Activity;
+
 
 
 public class WelcomeActivity extends AppCompatActivity {
-    Button loginButton;
-    Button registerButton;
+
+    Button logBtn;
+    Button regBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        registerButton = findViewById(R.id.regButton);
-        loginButton = findViewById(R.id.logButton);
+        logBtn = findViewById(R.id.logButton);
+        regBtn = findViewById(R.id.regButton);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        logBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new
-                        Intent(WelcomeActivity.this,LoginActivity.class);
+                        Intent(WelcomeActivity.this,MainActivity.class);
                 startActivity(i);
 
             }
-
 
         });
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        regBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new
-                        Intent(WelcomeActivity.this,RegisterActivity.class);
+                        Intent(WelcomeActivity.this,MainActivity.class);
                 startActivity(i);
 
             }
-
 
         });
     }
+
 }
