0a57ad16ff0fcafb75f41a256304d6f2a9e15641
KayL-1 <kyledelapena45@gmail.com>
2023-05-03 15:08:58 +0800

    sadadaa
    asdada

--- GroceryApp/app/src/main/java/com/example/groceryapp/ProfMyShopActivity.java
+++ GroceryApp/app/src/main/java/com/example/groceryapp/ProfMyShopActivity.java
@@ -6,15 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import androidx.cardview.widget.CardView;
 
 public class ProfMyShopActivity extends AppCompatActivity {
 
     ImageView backNav;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_prof_hehe);
+        setContentView(R.layout.activity_prof_myshop);
         backNav = findViewById(R.id.backNav);
 
         backNav.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +28,14 @@ public class ProfMyShopActivity extends AppCompatActivity {
 
             }
 
+
+            public void productButton(View v) {
+                Intent i = new
+                        Intent(ProfMyShopActivity.this, ProductActivity.class);
+                startActivity(i);
+            }
         });
+
+
     }
 }
